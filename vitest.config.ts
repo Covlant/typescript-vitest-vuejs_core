3062d363197b9e612a0496076f34f9bb3660d15b
sk <>
2025-04-20 19:10:23 +0530

    fix: update vitest.config

--- vitest.config.ts
+++ vitest.config.ts
@@ -34,19 +34,20 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       reporter: ['text', 'html'],
-      include: ['packages/*/src/**'],
+      // include: ['packages/*/src/**', ],
+      include: ['*'],
       exclude: [
-        // entries that are not really used during tests
+        // // entries that are not really used during tests
         // 'packages/vue-compat/**',
-        'packages/vue/src/dev.ts',
-        'packages/vue/src/runtime.ts',
-        // not testable during unit tests
-        'packages/runtime-core/src/profiling.ts',
-        'packages/runtime-core/src/featureFlags.ts',
-        'packages/runtime-core/src/customFormatter.ts',
-        // tested via e2e so no coverage is collected
-        'packages/runtime-core/src/hydrationStrategies.ts',
-        'packages/runtime-dom/src/components/Transition*',
+        // 'packages/vue/src/dev.ts',
+        // 'packages/vue/src/runtime.ts',
+        // // not testable during unit tests
+        // 'packages/runtime-core/src/profiling.ts',
+        // 'packages/runtime-core/src/featureFlags.ts',
+        // 'packages/runtime-core/src/customFormatter.ts',
+        // // tested via e2e so no coverage is collected
+        // 'packages/runtime-core/src/hydrationStrategies.ts',
+        // 'packages/runtime-dom/src/components/Transition*',
       ],
     },
   },
